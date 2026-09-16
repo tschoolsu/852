bfc07d8368f4a0301c9ea9c41fec2a2ae5574f3b
11430106 <11430106@tschool.tp.edu.tw>
2026-09-16 16:10:52 +0800

    Align T-Files visual style with student portal

--- cloud/app/layout.tsx
+++ cloud/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import './server-dialogs.css';
 import './auth-fields.css';
+import './portal-theme.css';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
